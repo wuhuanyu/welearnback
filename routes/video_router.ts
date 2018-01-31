216d0a6e41dec87d1438a170ccb2ba91273d23d6
wuhuanyu <1790949749@qq.com>
2018-01-31 21:36:13 +0800

    video

--- routes/video_router.ts
+++ routes/video_router.ts
@@ -40,4 +40,11 @@ router.get(/^\/([0-9]+)$/, applyErrorMW(async (req: express.Request, res: expres
     }
 }));
 
+
+
+router.get(/^\/list$/,applyErrorMW(async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+    let course_id=req.url_params['course_id'];
+    
+}));
+
 module.exports=router;
